55ece53abc85c52980d13d4033307910528f2be3
jhaaakash <aakashkumarjha295@gmail.com>
2024-10-29 01:30:46 +0530

    Merger after adding congif for swagger

--- src/main/java/com/crio/task_box_backend/config/OpenApiConfig.java
+++ src/main/java/com/crio/task_box_backend/config/OpenApiConfig.java
@@ -1,5 +1,15 @@
 package com.crio.task_box_backend.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
 @Configuration
 public class OpenApiConfig {
 
